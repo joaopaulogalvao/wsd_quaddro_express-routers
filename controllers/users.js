8539597fd74715f9f5a4950742b7d863f9605002
WebServices30.11a11.12 <webservices3011a1112@JobsMac9.local>
2015-12-08 22:12:41 -0200

    Almost end of class7

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let Users = require('../models/index.js').users; // não precisaria do index.js
+
 module.exports = {
 
 	list: list,
@@ -10,19 +12,37 @@ module.exports = {
 
 };
 
+function filterByPermission (array) {
+	//process
+	return processData;
+}
+
 function list (req, res) {
 		// body...
-	var users = [
-		{name: 'Seahawks'},
-		{name: '49ers'}
-	];
-
-	res.json(users); // if you do not pass anything it is 200
-
+	Users
+		.find({}) // vazio para pegar todos
+		.then((users)=>users)
+		.then(filterByPermission)
+		.catch(function(err){
+			console.log('Error ');
+		});
+		
+		//res.json(users); // if you do not pass anything it is 200
+		// .exec()(err, data) {
+		// 	if (err) return throw err;
+		// }) callback hell
 };
 
 function create (req, res) {
 	// body...
+	// user = new User(req.body);
+
+	//user
+		// .save()
+		// .then()
+
+	console.log(req.body);
+
 	res
 		.status(201)
 		.json({
